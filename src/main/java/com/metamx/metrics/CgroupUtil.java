98607d049db7f9b815f515f9886b34c8e548ea8f
Charles Allen <charles@allen-net.com>
2017-06-19 21:52:41 -0700

    Update copyright and minor formatting

--- src/main/java/com/metamx/metrics/CgroupUtil.java
+++ src/main/java/com/metamx/metrics/CgroupUtil.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Metamarkets Group Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.metamx.metrics;
 
 import com.metamx.common.StringUtils;
@@ -9,10 +24,11 @@ public class CgroupUtil
   public static final String SPACE_MATCH = Pattern.quote(" ");
   public static final String COMMA_MATCH = Pattern.quote(",");
   public static final String COLON_MATCH = Pattern.quote(":");
-  // TODO: switch to ProcessHandle.current().getPid() for java9
 
   /**
    * Returns the PID as a best guess. This uses methods that are not guaranteed to actually be the PID.
+   *
+   * TODO: switch to ProcessHandle.current().getPid() for java9 potentially
    *
    * @return the PID of the current jvm if available
    *
